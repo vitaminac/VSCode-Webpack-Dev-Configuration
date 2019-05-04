fa88f33b273033bb07de5be5aabe84aac7118a38
daolao <dalao1002@gmail.com>
2019-05-04 09:47:39 +0200

    redux project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-window.main = function () {
-    alert("Hello World");
-}
-
-window.websocket = require("./socket.js");
-
-require("./jqueryUpload.js");
-
-// require("./dplayer-config.js").load();
+Redox = require("./redox");
+Redox.host({
+    html: function () {
+        return `<h1>This is Redox init project</h1>`;
+    }
+}, $("#redox"));
